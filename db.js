7630166385722b83c586266c3fd79670ca9b84a5
sourav samanta <saifactplanet@gmail.com>
2023-10-30 19:49:16 +0530

    b

--- db.js
+++ db.js
@@ -2,4 +2,5 @@ const mongoose = require('mongoose');
 
 const connection = mongoose.connect('mongodb+srv://sourav:samanta@cluster0.u0c5mxy.mongodb.net/hospital?retryWrites=true&w=majority')
 
+
 module.exports = { connection }
